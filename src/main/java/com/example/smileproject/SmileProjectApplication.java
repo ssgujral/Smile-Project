50652fdd154c80dcfa5d45f22da872ce37f9d797
samgujral <71089547+samgujral@users.noreply.github.com>
2022-06-26 03:19:41 +0200

    First commit

---
+++ src/main/java/com/example/smileproject/SmileProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.smileproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmileProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmileProjectApplication.class, args);
+	}
+
+}
